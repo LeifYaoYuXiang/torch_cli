4838060c9cdf225e855bd6c6d80128c09e92aa29
Yuxiang Yao <yaoyuxiangyyx2009@126.com>
2023-06-16 22:26:36 +0800

    fix bugs in jit

--- deploy/jit.py
+++ deploy/jit.py
@@ -2,5 +2,5 @@ import torch
 
 
 def build_jit_model(model, example, save_filepath):
-    scripted_model = torch.jit.script(model)
-    scripted_model.save('model.pt')
+    scripted_model = torch.jit.script(model, example)
+    scripted_model.save(save_filepath)
