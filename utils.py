b11505a2be578990a66157ec6e7146136bfa6744
Yuxiang Yao <yaoyuxiangyyx2009@126.com>
2023-06-17 10:25:03 +0800

    fix comment in the utils file

--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 
-# 控制random随机种子
+# control randomness by random seeds
+# 通过随机种子控制随机性
 def seed_setting(seed_number):
     os.environ['PYTHONHASHSEED'] = str(seed_number)
     random.seed(seed_number)
@@ -18,10 +19,11 @@ def seed_setting(seed_number):
     torch.backends.cudnn.enabled = False
 
 
-# Summary Writer 生成以便于 visualization
+# record the results of the experiment via SummaryWriter
+# 通过SummaryWriter记录实验结果
 def get_summary_writer(log_filepath):
     """
-    在终端输入： tensorboard --logdir=log_filepath --port XXX
+    tensorboard --logdir=log_filepath --port XXX
     e.g. tensorboard --logdir=run --port 4444
     """
     time_stamp = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
@@ -30,7 +32,8 @@ def get_summary_writer(log_filepath):
     return log_writer, log_dir
 
 
-# 讲模型的训练参数加以保存
+# save the parameters of the experiment into one file
+# 将实验的训练参数保存到文件中
 def record_configuration(save_dir, configuration_dict: dict):
     file_name = os.path.join(save_dir, 'configuration.ini')
     write_config = configparser.ConfigParser()
@@ -38,12 +41,12 @@ def record_configuration(save_dir, configuration_dict: dict):
         write_config.add_section(config_key)
         for sub_config_key, sub_config_value in config_value.items():
             write_config.set(config_key, sub_config_key, str(sub_config_value))
-    # save configuration
     cfg_file = open(file_name, 'w')
     write_config.write(cfg_file)
     cfg_file.close()
 
 
+# load model parameters into one INI file
 # 加载ini文件中模型参数
 def load_model_configuration(filepath, section_name):
     write_config = configparser.ConfigParser()
@@ -52,16 +55,20 @@ def load_model_configuration(filepath, section_name):
     return model_config
 
 
-# 保存模型
+# save the whole model
+# 保存整套模型
 def save_model(model, save_filepath):
     torch.save(model, save_filepath)
 
 
-# 加载模型
+# load the whole model
+# 加载整套模型
 def load_model(load_filepath):
     model = torch.load(load_filepath)
     return model
 
+
+# read list from one file
 # 从文件中读取List文件
 def read_list_from_file(filepath):
     datalist = []
@@ -74,6 +81,7 @@ def read_list_from_file(filepath):
     return datalist
 
 
+# save list into file
 #  将List数据存入文件
 def write_list_to_file(filepath, list_content):
     with open(filepath, "a+") as f:
@@ -83,6 +91,7 @@ def write_list_to_file(filepath, list_content):
     return True
 
 
+# save data into one pickle file
 # 写入Pickle文件
 def write_data_to_pickle(data, filepath):
     fw = open(filepath, 'wb')
@@ -91,6 +100,7 @@ def write_data_to_pickle(data, filepath):
     return True
 
 
+# read data from one pickle file
 # 读取Pickle文件
 def read_data_from_pickle(filepath):
     fr = open(filepath, 'rb')
@@ -98,7 +108,8 @@ def read_data_from_pickle(filepath):
     return data
 
 
-# 分类问题在eval/test阶段判断模型输出结果
+# judge the model's output in the test/eval phase in the classification problem
+# 分类问题在eval/test阶段，判断模型输出结果
 def classification_softmax(output):
     _, y_pred = torch.max(output.data, 1)
     return y_pred
